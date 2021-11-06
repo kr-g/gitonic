9077687d34149d03f2b4e013c98bf0c5227586de
kr-g <k.r.goger@gmail.com>
2021-11-06 18:32:03 +0100

    removed tk cmd

--- gitonic/tile/core.py
+++ gitonic/tile/core.py
@@ -66,45 +66,6 @@ ON_UNSELECT = "on_unselect"
 ON_CHANGE = "on_change"
 
 
-class TkCmd(object):
-    class Options(object):
-        def __init__(self, opts):
-            self.__dict__.update(opts)
-
-    def start(
-        self,
-        parent,
-        command=None,
-        tout=1000,
-        **opts,
-    ):
-        self.parent = parent
-        self.tout = tout
-        self.rc = None
-        self._stop = None
-        self._cmd = command
-        self.opts = TkCmd.Options(opts) if opts and type(opts) == dict else opts
-        self._disp(True)
-        return self
-
-    def _disp(self, schedule=False):
-        if schedule:
-            self.parent.after(self.tout, self._disp)
-            return
-        if self._stop:
-            self.rc = self._stop
-            return
-        self.rc = self.run()
-        if self.rc is None:
-            self._disp(True)
-
-    def run(self):
-        """overload this, or use command paramter in start"""
-        if self._cmd:
-            return self._cmd(self)
-        return 0
-
-
 class TkMixin(object):
     def init(self, root=None):
         self.tk = root
