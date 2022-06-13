58da9e071bb4c3a0d72df5699a6c8d5f95145e72
kr-g <k.r.goger@gmail.com>
2022-06-13 06:05:45 +0200

    reworked

--- run_qs_tool.sh
+++ run_qs_tool.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 
-rm mpycntrl/ MicroWebSrv2/ license/ -rf
 
 black .
 flake8 --config flake8.cfg
 python3 -m unittest -v
 
 
-# python3 -m pygitgrab  -f web -f cntrl
-
-
-for f in build dist *.egg-info; do 
+for f in build, dist, *.egg-info, __pycache__ ; do 
     echo remove $f
-    rm $f -rf
+    find . -name $f | xargs rm -rf  
 done
-
-find . -name __pycache__ | xargs rm -rf  
-
 
