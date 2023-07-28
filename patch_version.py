e5769300d8c85cc3554d4f06de21c8dfcfb96c5a
kr-g <k.r.goger+github@gmail.com>
2023-07-28 10:06:12 +0200

    patch version

--- patch_version.py
+++ patch_version.py
@@ -5,12 +5,13 @@ from gitonic.const import VERSION
 with open("README.md") as f:
     c = f.read()
 
-par = "VERSION[ /t]*=[ /t](.*)$"
+par = "VERSION[ /t]*=[ /t]*(.*)[ /t]*$"
 
 matches = re.finditer(par, c, re.MULTILINE)
 
 for m in matches:
     c = c.replace(m.groups()[0], VERSION)
+    print("replaced", m.groups()[0])
 
 with open("README.md", "w") as f:
     f.write(c)
