cd16abd21eddbc107dfd778bc62d149c9ba4559c
kr-g <k.r.goger+github@gmail.com>
2025-10-19 10:45:36 +0200

    update

--- install_linux.sh
+++ install_linux.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 # create virtual environment 
+
 cd ~
 python3 -m venv gitonic
 
-# install gitonic with all extras
-~/gitonic/bin/pip install gitonic[PEP08,MELD]
+cd gitonic
+
+source ./bin/activate
+
+# install gitonic with all pep08 and meld
+
+pip3 install gitonic[PEP08,MELD]
